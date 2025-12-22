33bcb2a5b2da7766e379528cdcf64e8846c173c3
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2025-12-22 20:24:54 +0530

    Display blogs.java removed comment out parts and in bookings.html removed javascript part

--- src/main/java/rentalcompany/management/service/DisplayBookings.java
+++ src/main/java/rentalcompany/management/service/DisplayBookings.java
@@ -17,13 +17,6 @@ public class DisplayBookings extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        /*RentalCompanyBookings b1 = new RentalCompanyBookings();
-
-        b1.setBookingId(101);
-        b1.setStatus("Active");
-        b1.setTotalAmount(20000);
-        b1.setPaymentStatus("Paid");
-        b1.setCustomerName("Ruwan");*/
 
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
